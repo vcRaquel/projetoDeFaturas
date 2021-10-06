b68dc7386159ac58cc3df4cc2c9a1717ad85e8ec
Raquel Leão <vc.raquel@gmail.com>
2021-10-06 20:59:09 -0300

    modelar classe Consumidor

---
+++ src/Consumidor.java
@@ -0,0 +1,42 @@
+public class Consumidor {
+    private String nome;
+    private String email;
+
+    //construtores
+    public Consumidor() {
+
+    }
+
+    public Consumidor(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    //getters e setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n O nome do consumidor é: " + getNome());
+        retorno.append("O e-mail do consumidor é: " + getEmail());
+        return retorno.toString();
+    }
+}
