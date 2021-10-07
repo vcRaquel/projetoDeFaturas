27ab00bce773808bc8c5ff34014f72aaa0e17c6f
Raquel Leão <vc.raquel@gmail.com>
2021-10-06 21:37:16 -0300

    capturar dados

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,12 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
     //linha de frente, apresentar menus e receber dados
+
+    private static Scanner capturarDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
