3d5e924e91b92ce331b31567ef1c6dd47ff3f4d2
Raquel Leão <vc.raquel@gmail.com>
2021-10-06 22:05:03 -0300

    pesquisar consumidor por email - Exception

--- src/br/com/zup/ServicoConsumidor.java
+++ src/br/com/zup/ServicoConsumidor.java
@@ -7,16 +7,26 @@ public class ServicoConsumidor {
     private static List<Consumidor> consumidores = new ArrayList<>();
 
     //validar email
-    public static void validarEmail(String email){
-        if (!email.contains("@")){
+    public static void validarEmail(String email) {
+        if (!email.contains("@")) {
             System.out.println("Email inválido");
         }
     }
+
     //cadastrar consumidor
-public static Consumidor cadastrarConsumidor(String nome, String email){
+    public static Consumidor cadastrarConsumidor(String nome, String email) {
         validarEmail(email);
         Consumidor consumidor = new Consumidor(nome, email);
         return consumidor;
-}
+    }
+
     //pesquisar consumidor por email
+    public static Consumidor buscarConsumidorPorEmail(String email) throws Exception { //para avisar que poderá ter uma exceção
+        for (Consumidor consumidorReferencia : consumidores) {
+            if (consumidorReferencia.getEmail().equals(email)) {
+                return consumidorReferencia;
+            }
+        }//para dizer ode a exceção pode acontecer
+        throw new Exception("Consumidor não encontrado");
+    }
 }
