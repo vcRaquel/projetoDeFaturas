0f80cabb028b2d524eeed6aab45650dcb47c86e3
Raquel Leão <vc.raquel@gmail.com>
2021-10-06 21:19:48 -0300

    modelar classe ServicoConsumidor

---
+++ src/br/com/zup/ServicoConsumidor.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicoConsumidor {
+    private static List<Consumidor> consumidores = new ArrayList<>();
+}
