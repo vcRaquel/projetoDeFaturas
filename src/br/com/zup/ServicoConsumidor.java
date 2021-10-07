1758e8262d2abd938f6ce0c5dff0d312cbce7def
Raquel Leão <vc.raquel@gmail.com>
2021-10-06 21:28:52 -0300

    validar se email contém @

--- src/br/com/zup/ServicoConsumidor.java
+++ src/br/com/zup/ServicoConsumidor.java
@@ -5,4 +5,14 @@ import java.util.List;
 
 public class ServicoConsumidor {
     private static List<Consumidor> consumidores = new ArrayList<>();
+
+    //validar email
+    public static void validarEmail(String email){
+        if (email.contains("@")){
+            System.out.println("Email inválido");
+        }
+    }
+    //cadastrar consumidor
+
+    //pesquisar consumidor por email
 }
