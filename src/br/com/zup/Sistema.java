7ff5cc3714fab6b807a9c40e875dc57265c0a3c0
Raquel Leão <vc.raquel@gmail.com>
2021-10-06 22:16:19 -0300

    completar cadastro de Fatura por método homônimo no Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
 public class Sistema {
     //linha de frente, apresentar menus e receber dados
 
-    private static Scanner capturarDados(String mensagem){
+    private static Scanner capturarDados(String mensagem) {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("Digite 1 para cadastrar um consumidor");
         System.out.println("Digite 2 para cadastrar uma fatura");
         System.out.println("Digite 3 para sair");
     }
 
-    public static Consumidor cadastrarConsumidor(){
+    public static Consumidor cadastrarConsumidor() {
         String nome = capturarDados("Digite o nome do consumidor: ").nextLine();
         String email = capturarDados("Digite o email do consumidor").nextLine();
         //chama o método c mesmo nome da classe de servicoConsumidor (mesmo nome, pois são partes do mesmo objetivo)
         return ServicoConsumidor.cadastrarConsumidor(nome, email);
+    }
+
+    public static Fatura cadastrarFatura() throws Exception {
+        String email = capturarDados("\n Digite o email do consumidor: ").nextLine();
+        double valor = capturarDados("\n Digite o valor da fatura: ").nextInt();
+        String dataVencimento = capturarDados("\n Digite a data de vencimento da fatura: ").nextLine();
+        return ServicoFatura.cadastrarFatura(email, valor, dataVencimento);
     }
 }
